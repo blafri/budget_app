310fe26018d67614e91816a5f4c18e857336baec
Blayne Farinha <blayne.farinha@gmail.com>
2015-07-13 17:57:03 +0000

    Setup the app to send email using Sendgrid Service

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :test
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
@@ -39,4 +39,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Configure Action Mailer
+  config.action_mailer.default_url_options = { host: 'budget-app-112393.nitrousapp.com', port: 3000 }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'no-reply@budget-app-112393.nitrousapp.com' }
 end
